9be5708ae3678f02301edebb5644610b061bf08a
Victor Jacobs <victor.jacobs@me.com>
2015-12-05 17:25:39 +0100

    Solution for 1.2

--- 1.notQuiteLisp/main.hs
+++ 1.notQuiteLisp/main.hs
@@ -1,6 +1,19 @@
 main = do
     content <- readFile "data.txt"
-    putStrLn $ show $ notQuiteLisp content
+    putStrLn $ show $ last $ floors content
+    putStrLn $ show $ basement content
 
-notQuiteLisp :: String -> Int
-notQuiteLisp xs = sum [if x == '(' then 1 else -1 | x <- xs]
+-- Simplest list comprehension solution
+finalFloor :: String -> Int
+finalFloor str = sum [if x == '(' then 1 else -1 | x <- str]
+
+floors :: String -> [Int]
+floors str = scanl newFloor 0 str
+
+newFloor :: Int -> Char -> Int
+newFloor prevFloor c
+    | c == '(' = prevFloor + 1
+    | otherwise = prevFloor -1
+
+basement :: String -> Int
+basement str = length $ fst $ span (>= 0) (floors str)
