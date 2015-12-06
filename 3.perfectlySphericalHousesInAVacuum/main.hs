92470a66074a11441376bae8184cf913faa8c86c
Victor Jacobs <victor.jacobs@me.com>
2015-12-06 12:55:03 +0100

    Day three

---
+++ 3.perfectlySphericalHousesInAVacuum/main.hs
@@ -0,0 +1,30 @@
+import qualified Data.Map.Strict as Map
+
+type Position = (Int, Int)
+type State = (Position, Map.Map Position Int)
+
+main = do
+    content <- readFile "data.txt"
+    let initialState = ((0, 0), Map.singleton (0, 0) 1)
+    let (_, result) = foldl step initialState content
+    putStrLn $ show $ Map.size result
+
+step :: State -> Char -> State
+step (pos, map) c = (newPos, newMap)
+    where
+        newPos = nextPosition pos c
+        newMap = Map.alter updateMap newPos map
+
+updateMap :: Maybe Int -> Maybe Int
+updateMap Nothing = Just 1
+updateMap (Just count) = Just $ count + 1
+
+-- List of all positions visited
+positions :: String -> [Position]
+positions str = scanl nextPosition (0, 0) str
+
+nextPosition :: Position -> Char -> Position
+nextPosition (x, y) '^' = (x, y+1)
+nextPosition (x, y) '>' = (x+1, y)
+nextPosition (x, y) '<' = (x-1, y)
+nextPosition (x, y) 'v' = (x, y-1)
