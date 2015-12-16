e3fb837cf38b82c2362bf37bd9441c7a0900bf93
Victor Jacobs <victor.jacobs@me.com>
2015-12-16 23:10:07 +0100

    First stab at day six

--- 5.doesntHeHaveInternElvesForThis/part2.hs
+++ 5.doesntHeHaveInternElvesForThis/part2.hs
@@ -13,15 +13,16 @@ niceStrings strs = filter (isNiceString (Map.empty, 0)) strs
 isNiceString :: State -> String -> Bool
 -- Base case: at least 3 vowels and at least one occurence of 1 letter twice in a row
 isNiceString (pairs, nbOfRepeats) []
-    | (Map.size $ Map.filter (>= 2) pairs) >= 1 && nbOfRepeats >= 1 = True
+    | (Map.size $ Map.filter (>= 2) pairs) == 1 && nbOfRepeats >= 1 = True
     | otherwise = False
 -- Repeats separated by a letter
-isNiceString (pairs, nbOfRepeats) str@(sa:sb:sc:_)
-    | sa == sc && sa == sb = isNiceString (newPairs, nbOfRepeats + 1) (tail str)
-    | sa == sc = isNiceString (pairs, nbOfRepeats + 1) (tail str)
-    | sa == sb = isNiceString (newPairs, nbOfRepeats) (tail str)
+isNiceString (pairs, nbOfRepeats) str@(sa:_:sc:_)
+    | sa == sc = isNiceString (newPairs, nbOfRepeats + 1) (tail str)
+    | otherwise = isNiceString (newPairs, nbOfRepeats) (tail str)
         where newPairs = Map.alter updateMap (take 2 str) pairs
-isNiceString state str = isNiceString state (tail str)
+isNiceString (pairs, nbOfRepeats) str@(_:_:_) = isNiceString (newPairs, nbOfRepeats) (tail str)
+    where newPairs = Map.alter updateMap (take 2 str) pairs
+isNiceString state str@(_:_) = isNiceString state (tail str)
 
 updateMap :: Maybe Int -> Maybe Int
 updateMap Nothing = Just 1
---
+++ 6.probablyAFireHazard/part1.hs
@@ -0,0 +1,54 @@
+import qualified Data.Map.Strict as Map
+import Data.List.Split
+
+type State = Map.Map Pos Bool
+type Pos = (Int, Int)
+
+main = do
+    content <- readFile "data.txt"
+    let lights = foldl processLine Map.empty (lines content)
+    putStrLn $ show $ Map.size $ Map.filter id lights
+
+
+processLine :: State -> String -> State
+processLine state instruction = processInstruction (words instruction) state
+
+processInstruction :: [String] -> State -> State
+processInstruction ("turn":value:from:_:to:_) state
+    | value == "on" = turnOffRectangle fromPos toPos state
+    | value == "off" = turnOffRectangle fromPos toPos state
+        where fromPos = posFromString from
+              toPos = posFromString to
+processInstruction ("toggle":from:_:to:_) state =
+    toggleRectangle (posFromString from) (posFromString to) state
+
+turnOnRectangle :: Pos -> Pos -> State -> State
+turnOnRectangle pos1 pos2 state = executeOnRectangle turnOnLight pos1 pos2 state
+
+turnOffRectangle :: Pos -> Pos -> State -> State
+turnOffRectangle pos1 pos2 state = executeOnRectangle turnOffLight pos1 pos2 state
+
+toggleRectangle :: Pos -> Pos -> State -> State
+toggleRectangle pos1 pos2 state = executeOnRectangle toggleLight pos1 pos2 state
+
+executeOnRectangle :: (Maybe Bool -> Maybe Bool) -> Pos -> Pos -> State -> State
+executeOnRectangle f (x1, y1) (x2, y2) state =
+    foldl (curry $ mapAlter f) state [(x, y) | x <- [x1..x2], y <- [y1..y2]]
+
+mapAlter :: (Maybe Bool -> Maybe Bool) -> (State, Pos) -> State
+mapAlter f (state, pos) = Map.alter f pos state
+
+
+turnOnLight :: Maybe Bool -> Maybe Bool
+turnOnLight _ = Just True
+
+turnOffLight :: Maybe Bool -> Maybe Bool
+turnOffLight _ = Just False
+
+toggleLight :: Maybe Bool -> Maybe Bool
+toggleLight Nothing = Just True
+toggleLight (Just b) = Just $ not b
+
+posFromString :: String -> Pos
+posFromString str = (x, y)
+    where [x, y] = map read $ splitOn "," str
