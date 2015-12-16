8c553b30389c66b9914118f6b61a6b1740f95ca5
Victor Jacobs <victor.jacobs@me.com>
2015-12-16 23:44:39 +0100

    Part 2 almost working

--- 6.probablyAFireHazard/part2.hs
+++ 6.probablyAFireHazard/part2.hs
@@ -2,13 +2,13 @@ import qualified Data.Map.Strict as Map
 import Data.List.Split
 import Data.List
 
-type State = Map.Map Pos Bool
+type State = Map.Map Pos Int
 type Pos = (Int, Int)
 
 main = do
     content <- readFile "data.txt"
     let lights = foldl' processLine Map.empty (lines content)
-    putStrLn $ show $ Map.size $ Map.filter id lights
+    putStrLn $ show $ Map.foldl + 0 lights
 
 processLine :: State -> String -> State
 processLine state instruction = processInstruction (words instruction) state
@@ -31,22 +31,24 @@ turnOffRectangle pos1 pos2 state = executeOnRectangle turnOffLight pos1 pos2 sta
 toggleRectangle :: Pos -> Pos -> State -> State
 toggleRectangle pos1 pos2 state = executeOnRectangle toggleLight pos1 pos2 state
 
-executeOnRectangle :: (Maybe Bool -> Maybe Bool) -> Pos -> Pos -> State -> State
+executeOnRectangle :: (Maybe Int -> Maybe Int) -> Pos -> Pos -> State -> State
 executeOnRectangle f (x1, y1) (x2, y2) state =
     foldl' (curry $ mapAlter f) state [(x, y) | x <- [x1..x2], y <- [y1..y2]]
 
-mapAlter :: (Maybe Bool -> Maybe Bool) -> (State, Pos) -> State
+mapAlter :: (Maybe Int -> Maybe Int) -> (State, Pos) -> State
 mapAlter f (state, pos) = Map.alter f pos state
 
-turnOnLight :: Maybe Bool -> Maybe Bool
-turnOnLight _ = Just True
+turnOnLight :: Maybe Int -> Maybe Int
+turnOnLight Nothing = Just 1
+turnOnLight (Just b) = Just (b + 1)
 
-turnOffLight :: Maybe Bool -> Maybe Bool
-turnOffLight _ = Just False
+turnOffLight :: Maybe Int -> Maybe Int
+turnOffLight Nothing = Just (-1)
+turnOffLight (Just b) = Just (b - 1)
 
-toggleLight :: Maybe Bool -> Maybe Bool
-toggleLight Nothing = Just True
-toggleLight (Just b) = Just $ not b
+toggleLight :: Maybe Int -> Maybe Int
+toggleLight Nothing = Just 2
+toggleLight (Just b) = Just (b + 2)
 
 posFromString :: String -> Pos
 posFromString str = (x, y)
